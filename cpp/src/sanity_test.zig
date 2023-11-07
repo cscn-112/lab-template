aa03170a5806481c33978f0d8b688da9801ee176
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-11-07 18:49:49 -0500

    test(C++): clean up sanity test

--- cpp/src/sanity_test.zig
+++ cpp/src/sanity_test.zig
@@ -1,26 +1,44 @@
 const std = @import("std");
 
-pub fn sanity_test() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
 test "simple test" {
-    try sanity_test();
+    var sources = std.ArrayList([]const u8).init(std.testing.allocator);
+    defer sources.deinit();
 
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    // Search for all C/C++ files in `src/lib` and add them
+    search: {
+        const base_path: []const u8 = "src/lib";
+        const allowed_exts = [_][]const u8{ ".c", ".cpp", ".cxx", ".c++", ".cc" };
+
+        var dir = std.fs.cwd().openIterableDir("../" ++ base_path, .{}) catch |err| switch (err) {
+            std.fs.Dir.OpenError.FileNotFound => break :search,
+            else => return err,
+        };
+
+        var walker = try dir.walk(std.testing.allocator);
+        defer walker.deinit();
+
+        while (try walker.next()) |entry| {
+            const ext = std.fs.path.extension(entry.basename);
+            const include_file = for (allowed_exts) |e| {
+                if (std.mem.eql(u8, ext, e))
+                    break true;
+            } else false;
+
+            if (include_file) {
+                var path_vec = std.ArrayList(u8).init(std.testing.allocator);
+                defer path_vec.deinit();
+
+                try path_vec.appendSlice(base_path ++ "/");
+                try path_vec.appendSlice(entry.path);
+
+                const full_path = try std.testing.allocator.dupe(u8, path_vec.items);
+
+                try sources.append(full_path);
+            }
+        }
+    }
+
+    for (sources.items) |value| {
+        std.debug.print("path: {s}", .{value});
+    }
 }
